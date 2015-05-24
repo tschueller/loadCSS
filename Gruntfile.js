635be6c185a3d2ae62f3ec39183fd4efda2882c0
Thorsten Schüller <thorsten-nl@schueller.me>
2015-05-24 11:45:54 +0200

    Added uglify to create a minimized version

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,37 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-	grunt.initConfig({
-    jshint: {
-			all: {
-				options: {
-					jshintrc: ".jshintrc"
-				},
+    grunt.initConfig({
+        jshint: {
+            all: {
+                options: {
+                    jshintrc: ".jshintrc"
+                },
 
-				src: ['Gruntfile.js', '*.js']
-			}
-		}
-  });
-
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', 'jshint');
+                src: ['Gruntfile.js', '*.js']
+            }
+        },
+        uglify: {
+            loadCSS : {
+                options: {
+                    preserveComments: 'some',
+                    mangle: false,
+                    beautify: false,
+                    },            
+                   files: { 'dest/loadCSS.min.js': 'loadCSS.js'}
+            },
+            onloadCSS : {
+                options: {
+                    mangle: false,
+                    beautify: false,
+                    },           
+                files: { 'dest/onloadCSS.min.js': 'onloadCSS.js'}
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default' , ['jshint', 'uglify']);
+    grunt.registerTask('minimize' , ['uglify']);
 };
